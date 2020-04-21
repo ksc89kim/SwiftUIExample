c86bf816f5cff437eb7bc6c4b6d134a8d004ac54
kim sunchul <ksc89kim@naver.com>
2020-04-21 15:48:26 +0900

    1. How to create static labels with a TextView

--- TextAndImage/TextAndImage/ContentView.swift
+++ TextAndImage/TextAndImage/ContentView.swift
@@ -10,7 +10,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        List {
+//            줄수 지정 Modifier
+            Text("Hello, World!")
+                       .lineLimit(3)
+            
+//            줄수 지정 및 ... 위치 수정 Modifier
+                Text("This is an extremely long textbstring that will never fit even the widest of Phones")
+                .lineLimit(1)
+                .truncationMode(.middle)
+            
+        }
+       
+        
+        
     }
 }
 
