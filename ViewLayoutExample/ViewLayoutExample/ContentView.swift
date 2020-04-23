69e6b09773f0b360277c9e7d97e0b93dcbf668cb
kim sunchul <ksc89kim@naver.com>
2020-04-23 16:41:02 +0900

    1. How to create stacks using VStack and HStack 2. How to customize stack layouts with alignment and spacing

---
+++ ViewLayoutExample/ViewLayoutExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewLayoutExample
+//
+//  Created by kim sunchul on 2020/04/23.
+//  Copyright © 2020 kim sunchul. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        StackExampleView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
