9bd03e03937b286eb8f89a667461b9c82a07e882
kim sunchul <ksc89kim@naver.com>
2020-05-01 15:55:00 +0900

    RespondingToEvents 프로젝트 생성

---
+++ RespondingToEvents/RespondingToEvents/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RespondingToEvents
+//
+//  Created by kim sunchul on 2020/05/01.
+//  Copyright © 2020 kim sunchul. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
