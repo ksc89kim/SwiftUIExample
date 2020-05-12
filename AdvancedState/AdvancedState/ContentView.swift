2ac66eea06fe12f16cd4feb5c0a3bd1184b4fbfd
kim sunchul <ksc89kim@naver.com>
2020-05-12 15:46:03 +0900

    - AdvancedState 프로젝트 추가

---
+++ AdvancedState/AdvancedState/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdvancedState
+//
+//  Created by kim sunchul on 2020/05/12.
+//  Copyright © 2020 kim sunchul. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
