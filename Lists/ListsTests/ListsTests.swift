ee959ddbf01b57c7dbc3f283418204a866db0613
kim sunchul <ksc89kim@naver.com>
2020-05-22 15:53:49 +0900

    0. Lists 프로젝트 생성 1. How to create a list of static items 2. How to create a list of dynamic items 3. How to let users delete rows from a list 4. How to let users move rows in a list

---
+++ Lists/ListsTests/ListsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ListsTests.swift
+//  ListsTests
+//
+//  Created by kim sunchul on 2020/05/22.
+//  Copyright © 2020 kim sunchul. All rights reserved.
+//
+
+import XCTest
+@testable import Lists
+
+class ListsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
