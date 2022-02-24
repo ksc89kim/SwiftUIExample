a5d7de8068e6a26bb060333de2a7932bc142a9d5
kim sunchul <ksc89kim@naver.com>
2022-02-24 22:17:30 +0900

    Presenting views 프로젝트 생성

---
+++ PresentingViews/PresentingViews/PresentingViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentingViewsApp.swift
+//  PresentingViews
+//
+//  Created by kim sunchul on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PresentingViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
